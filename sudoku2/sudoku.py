f40a65141c4e362d107cfad8c40011ef0d92960a
oliveringle <oliver.ingle@gmail.com>
2025-06-06 15:43:52 +0200

    New wfc algo for new sudoku engine and statistical tests

--- sudoku2/sudoku.py
+++ sudoku2/sudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import random
 
 def reduce(L, x):
     N = []
@@ -120,6 +121,7 @@ class Sudoku:
 def wfc(S):
     s = S.s
     tag = True
+    contraditcion = False
     while tag:
         tag = False
         min_entropy = s
@@ -129,38 +131,75 @@ def wfc(S):
                 entropies = S.entropies[i][j]
                 n_entropies = len(entropies)
                 if n_entropies == 0:
+                    if S.grid[i][j] == None:
+                        S.force_place(i, j, "\033[31;1mC\033[0m")
+                        contraditcion = True
                     continue
-                if n_entropies == 1:
+                elif n_entropies == 1:
                     S.place_and_reduce(i, j, entropies[0])
                     tag = True
+                    break
+                elif n_entropies == min_entropy:
+                    min_entropy_coords.append((i, j))
+                elif n_entropies < min_entropy:
+                    min_entropy = n_entropies
+                    min_entropy_coords = [(i, j)]
+            if tag or contraditcion:
+                break
+        if tag or min_entropy_coords == [] or contraditcion:
+            # S.update_view()
+            continue
+        r = random.randint(0, len(min_entropy_coords) - 1)
+        (x, y) = min_entropy_coords[r]
+        r2 = random.randint(0, min_entropy - 1)
+        el = S.entropies[x][y][r2]
+        S.place_and_reduce(x, y, el)
+        tag = True
+        # input()
+        # S.update_view()
+
+    return not contraditcion
+
+
+
 
 
 # ---- None marker ----
 N = None
 # ---- ----------- ----
 
+# --------------------- Test 1 ----------------------
 
-S1 = Sudoku(2)
-# print(S1.grid)
-# print(S1.entropies)
+# S1 = Sudoku(2)
+# # print(S1.grid)
+# # print(S1.entropies)
 
-# S1.force_infere([
-#     [0, 1, N, 1],
+# # S1.force_infere([
+# #     [0, 1, N, 1],
+# #     [1, N, N, N],
+# #     [N, N, N, N],
+# #     [1, N, N, N]
+# # ])
+
+# S1.infere_and_reduce([
+#     [0, 3, N, 1],
 #     [1, N, N, N],
 #     [N, N, N, N],
-#     [1, N, N, N]
+#     [N, N, N, N]
 # ])
+# wfc(S1)
+# S1.view()
 
-S1.view()
-S1.infere_and_reduce([
-    [0, 3, N, 1],
-    [1, N, N, N],
-    [N, N, N, N],
-    [N, N, N, N]
-])
-input()
-S1.update_view()
-input()
-wfc(S1)
 
-S1.update_view()
+# --------------------- Test 2 ----------------------
+success = 0
+n = 100000
+for i in range(n):
+    sys.stdout.write("\x1b[1A\x1b[2K")
+    S1 = Sudoku(3)
+    if wfc(S1):
+        success += 1
+    print(f"\033[94m{np.round(i/n * 100)}%\033[0m")
+
+pourcent = success / n * 100
+print(f"Le sudoku est resolu \033[92m{pourcent}%\033[0m du temps")
